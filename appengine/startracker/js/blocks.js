8856a147a87408412ae33a080062d840612a6d32
U-Sandra-PC\Sandra <vanaheim.wen@gmail.com>
2015-04-07 16:45:50 +0800

    First version of web pages, including homepage, star_blockly, star_scratch.

---
+++ appengine/startracker/js/blocks.js
@@ -0,0 +1,35 @@
+/**
+ * Blockly Games: Startracker
+ *
+ 
+/**
+ * 
+ * @author weifen@mijotech.net
+ */
+'use strict'; /* var only used in this name space */
+
+goog.provide('Startracker.Blocks');
+
+/* use blocky block */
+goog.require('Blockly');
+goog.require('BlocklyGames');
+goog.require('Blockly.JavaScript');
+goog.require('Blockly.Blocks.colour');
+goog.require('Blockly.JavaScript.colour');
+goog.require('Blockly.Blocks.lists');
+goog.require('Blockly.JavaScript.lists');
+goog.require('Blockly.Blocks.logic');
+goog.require('Blockly.JavaScript.logic');
+goog.require('Blockly.Blocks.loops');
+goog.require('Blockly.JavaScript.loops');
+goog.require('Blockly.Blocks.math');
+goog.require('Blockly.JavaScript.math');
+goog.require('Blockly.Blocks.procedures');
+goog.require('Blockly.JavaScript.procedures');
+goog.require('Blockly.Blocks.texts');
+goog.require('Blockly.JavaScript.texts');
+goog.require('Blockly.Blocks.variables');
+goog.require('Blockly.JavaScript.variables');
+goog.require('BlocklyGames');
+
+
